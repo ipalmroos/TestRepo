7a37af0b27e08a917fc4e82462e731d6ac083d9e
ipalmroos <ian.palmroos@gmail.com>
2024-09-25 20:46:51 -0500

    Now with dancing chicken

--- SillyBirdProject/src/main/java/edu/augustana/Main.java
+++ SillyBirdProject/src/main/java/edu/augustana/Main.java
@@ -1,7 +1,10 @@
 package edu.augustana;
 
 public class Main {
+    public static Chicken chicken = new Chicken();
+
     public static void main(String[] args) {
         System.out.println("Hello bird world!");
+        chicken.dance();
     }
 }
